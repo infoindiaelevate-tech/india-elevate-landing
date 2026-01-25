4ed33f44bb9e4d21fdcffcbfd38b82331aae9cdc
SOUMIK CHATTTERJEE <soumikc137@gmail.com>
2026-01-26 04:04:09 +0530

    first deploy ready

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",  // 👈 1. Generates static HTML/CSS/JS
+  images: {
+    unoptimized: true,
+  }
 };
 
 export default nextConfig;
